6d47f2d794f3b97fadac9f282d338f94119ebd0b
SaukiFutaki <utaki.sauki19@gmail.com>
2024-08-03 22:23:24 +0700

    +1 looping exercise

--- looping/golang-looping-cp-4-v2/main.go
+++ looping/golang-looping-cp-4-v2/main.go
@@ -1,37 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func EmailInfo(email string) string {
-	var domain string
-	var tld string
+	// var domain string
+	// var tld string
 
-	index := -1
-	for i := 0; i < len(email); i++ {
+	// index := -1
+	// for i := 0; i < len(email); i++ {
 		
-		fmt.Print(string(email[i]))
-		if email[i] == '@' {
-			// domain = email[i+1:]
-			// break
-			index = i
-			break
-		}
+	// 	fmt.Print(string(email[i]))
+	// 	if email[i] == '@' {
+	// 		// domain = email[i+1:]
+	// 		// break
+	// 		index = i
+	// 		break
+	// 	}
 
 
+	// }
+
+	// dotIndex := -1
+	// for i := index+1; i < len(email); i++ {
+	// 	fmt.Print(string(email[i]))
+	// 	if email[i] == '.' {
+	// 		dotIndex = i
+	// 		break
+	// 	}
+	// }
+
+	// domain = email[index+1:]
+	// tld = email[dotIndex+1:]
+	// return "domain: " + domain + " tld: " + tld + " index: " + string(index)
+
+	emailSplit := strings.Split(email, "@")
+	emailInfo := strings.Split(emailSplit[1], ".")
+	var TLD string
+	if len(emailInfo) > 2 {
+		TLD = strings.Join(emailInfo[1:], ".")
+	} else {
+		TLD = emailInfo[1]
 	}
-
-	dotIndex := -1
-	for i := index+1; i < len(email); i++ {
-		fmt.Print(string(email[i]))
-		if email[i] == '.' {
-			dotIndex = i
-			break
-		}
-	}
-
-	domain = email[index+1:]
-	tld = email[dotIndex+1:]
-	return "domain: " + domain + " tld: " + tld + " index: " + string(index)
+	fmt.Println(emailInfo)
+	fmt.Println(TLD)
+	fmt.Println(emailSplit)
+	return "Domain: " + emailInfo[0] + " dan"  + " TLD: " + TLD
 }
 
 // gunakan untuk melakukan debug
